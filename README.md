9b82089d3f83edcf54bde97eca85bc14d489b963
Bo Lu <lv.patrick@gmail.com>
2018-02-19 14:51:47 +1100

    add readme and update front page

---
+++ README.md
@@ -0,0 +1,14 @@
+This repository is source code of https://zbox.io website.
+
+Credit
+=========
+
+- [Hugo](https://gohugo.io)
+- [Semantic UI](https://semantic-ui.com)
+- [Flat Color Icon](https://github.com/icons8/flat-color-icons)
+- [Ruda](https://fonts.google.com/specimen/Ruda)
+
+License
+=======
+This source code is licensed under the Apache 2.0 License - see the [LICENSE](LICENSE)
+file for details.
--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,19 @@
 $(document).ready(function () {
   // create sidebar and attach to menu open
   $('.ui.sidebar').sidebar('attach events', '.toc');
+
+  // initial tabs in 'Get started' section
+  $('#start .menu .item').tab();
+
+  $('.scrollLink').click(function(event) {
+    //event.preventDefault();
+    var fromSidebar = $(this).parents(".sidebar").length > 0;
+    if (fromSidebar) {
+      $('.ui.sidebar').sidebar('toggle');
+    }
+    var tgt = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(tgt).offset().top
+    }, 500);
+  });
 });
